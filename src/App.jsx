c3a8ed899f5cc6bc2635a5bb2d33c8dde5691e4c
Ayush05m <ayush05m@gmail.com>
2024-10-01 23:48:36 +0530

    template

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import { About } from './pages/About';
+import { Home } from './pages/Home';
+import { Team } from './pages/Team';
+import { Event } from './pages/Event';
+import { Navbar } from './pages/Navbar';
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <main className="pt-0">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/events" element={<Event />} />
+          <Route path="/team" element={<Team />} />
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,css,jsx,tsx}",
+    ], theme: {
+        extend: {},
+    },
+    plugins: [],
+};
