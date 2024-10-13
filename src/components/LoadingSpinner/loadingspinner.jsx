470533ebf51fdd557ae5cd05ddb2391ffd67248f
Anupam Poddar <anupampoddar97@gmail.com>
2024-10-13 21:04:07 +0530

    loading spinner and PreLoader

---
+++ src/components/LoadingSpinner/loadingspinner.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import './loadingspinner.css';
+
+const LoadingSpinner = () => {
+  return (
+    <div className="loading-spinner">
+      <div className="spinner">
+        
+      </div>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
