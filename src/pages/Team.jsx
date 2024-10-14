32e4af5f3165d062a55211feb88fbe341292533e
Ayush05m <72142971+Ayush05m@users.noreply.github.com>
2024-10-14 13:04:19 +0530

    team update

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export const Team = () => {
   return (
-    <div>Team</div>
+    <div>
+      <>
+        <Navbarshaurya />
+        <Landing />
+        <Ocs />
+        <Hcs />
+      </>
+    </div>
   )
 }
